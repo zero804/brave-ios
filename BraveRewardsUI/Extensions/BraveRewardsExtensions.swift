932b56c37c49aae8fa538236cb64c13b543356b1
Kyle Hickinson <git@kylehickinson.com>
2020-10-30 16:23:07 -0400

    Move new BR extension methods to Client

--- BraveRewardsUI/Extensions/BraveRewardsExtensions.swift
+++ BraveRewardsUI/Extensions/BraveRewardsExtensions.swift
@@ -15,45 +15,4 @@ extension BraveRewards {
     return DCDevice.current.isSupported
     #endif
   }
-  
-  /// Whether or not rewards is enabled
-  public var isEnabled: Bool {
-    get {
-      ledger.isWalletCreated && ledger.isEnabled && ads.isEnabled
-    }
-    set {
-      createWalletIfNeeded { [weak self] in
-        guard let self = self else { return }
-        self.ledger.isEnabled = newValue
-        self.ledger.isAutoContributeEnabled = newValue
-        self.ads.isEnabled = newValue
-      }
-    }
-  }
-  
-  public func createWalletIfNeeded(_ completion: @escaping () -> Void) {
-    if ledger.isWalletCreated {
-      completion()
-      return
-    }
-    if isCreatingWallet {
-      // completion block will be hit by previous call
-      return
-    }
-    isCreatingWallet = true
-    ledger.createWalletAndFetchDetails { [weak self] success in
-      self?.isCreatingWallet = false
-      completion()
-    }
-  }
-  
-  public var isCreatingWallet: Bool {
-    get { objc_getAssociatedObject(self, &AssociatedKeys.isCreatingWallet) as? Bool ?? false }
-    set { objc_setAssociatedObject(self, &AssociatedKeys.isCreatingWallet, newValue, .OBJC_ASSOCIATION_ASSIGN) }
-  }
 }
-
-private struct AssociatedKeys {
-  static var isCreatingWallet: Int = 0
-}
-  
---
+++ Client/Extensions/BraveRewardsExtensions.swift
@@ -0,0 +1,49 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import Foundation
+import BraveRewards
+
+extension BraveRewards {
+  /// Whether or not rewards is enabled
+  public var isEnabled: Bool {
+    get {
+      ledger.isWalletCreated && ledger.isEnabled && ads.isEnabled
+    }
+    set {
+      createWalletIfNeeded { [weak self] in
+        guard let self = self else { return }
+        self.ledger.isEnabled = newValue
+        self.ledger.isAutoContributeEnabled = newValue
+        self.ads.isEnabled = newValue
+      }
+    }
+  }
+  
+  public func createWalletIfNeeded(_ completion: @escaping () -> Void) {
+    if ledger.isWalletCreated {
+      completion()
+      return
+    }
+    if isCreatingWallet {
+      // completion block will be hit by previous call
+      return
+    }
+    isCreatingWallet = true
+    ledger.createWalletAndFetchDetails { [weak self] success in
+      self?.isCreatingWallet = false
+      completion()
+    }
+  }
+  
+  public var isCreatingWallet: Bool {
+    get { objc_getAssociatedObject(self, &AssociatedKeys.isCreatingWallet) as? Bool ?? false }
+    set { objc_setAssociatedObject(self, &AssociatedKeys.isCreatingWallet, newValue, .OBJC_ASSOCIATION_ASSIGN) }
+  }
+}
+
+private struct AssociatedKeys {
+  static var isCreatingWallet: Int = 0
+}
+  
