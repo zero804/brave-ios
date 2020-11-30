239c6a2f616139a783e9ea4e0d44a06bed8c646d
Kyle Hickinson <git@kylehickinson.com>
2020-11-30 15:57:04 -0500

    WIP: Widgets

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -20,6 +20,7 @@ import StoreKit
 import SafariServices
 import BraveUI
 import NetworkExtension
+import CoreData
 
 private let log = Logger.browserLogger
 
@@ -146,6 +147,8 @@ class BrowserViewController: UIViewController {
     private(set) var publisher: PublisherInfo?
     
     let vpnProductInfo = VPNProductInfo()
+    
+    private(set) lazy var navigationHelper = BrowserNavigationHelper(self)
 
     init(profile: Profile, tabManager: TabManager, crashedLastSession: Bool,
          safeBrowsingManager: SafeBrowsing? = SafeBrowsing()) {
@@ -401,7 +404,21 @@ class BrowserViewController: UIViewController {
                 }
             }
         }
+        
+        if #available(iOS 14.0, *) {
+            widgetBookmarksFRC = Bookmark.frc(forFavorites: true, parentFolder: nil)
+            widgetBookmarksFRC?.fetchRequest.fetchLimit = 16
+            widgetBookmarksFRC?.delegate = self
+            try? widgetBookmarksFRC?.performFetch()
+            
+            if !FavoritesWidgetData.dataExists {
+                updateWidgetFavoritesData()
+            }
+        }
     }
+    
+    private var widgetBookmarksFRC: NSFetchedResultsController<Bookmark>?
+    private var widgetFaviconFetchers: [FaviconFetcher] = []
     
     let deviceCheckClient: DeviceCheckClient?
     
@@ -1689,7 +1706,7 @@ class BrowserViewController: UIViewController {
         }
     }
 
-    fileprivate func presentActivityViewController(_ url: URL, tab: Tab? = nil, sourceView: UIView?, sourceRect: CGRect, arrowDirection: UIPopoverArrowDirection) {
+    func presentActivityViewController(_ url: URL, tab: Tab? = nil, sourceView: UIView?, sourceRect: CGRect, arrowDirection: UIPopoverArrowDirection) {
         let helper = ShareExtensionHelper(url: url, tab: tab)
         
         let findInPageActivity = FindInPageActivity() { [unowned self] in
@@ -2248,11 +2265,9 @@ extension BrowserViewController: TopToolbarDelegate {
         let popover = PopoverController(contentController: container, contentSizeBehavior: .preferredContentSize)
         popover.present(from: topToolbar.locationView.shieldsButton, on: self)
     }
-    
-    // TODO: This logic should be fully abstracted away and share logic from current MenuViewController
-    // See: https://github.com/brave/brave-ios/issues/1452
+  
     func topToolbarDidTapBookmarkButton(_ topToolbar: TopToolbarView) {
-        showBookmarkController()
+        navigationHelper.openBookmarks()
     }
     
     func topToolbarDidTapBraveRewardsButton(_ topToolbar: TopToolbarView) {
@@ -2287,31 +2302,7 @@ extension BrowserViewController: ToolbarDelegate {
     }
     
     func tabToolbarDidPressShare() {
-        func share(url: URL) {
-            presentActivityViewController(
-                url,
-                tab: url.isFileURL ? nil : tabManager.selectedTab,
-                sourceView: view,
-                sourceRect: view.convert(topToolbar.menuButton.frame, from: topToolbar.menuButton.superview),
-                arrowDirection: [.up]
-            )
-        }
-        
-        guard let tab = tabManager.selectedTab, let url = tab.url else { return }
-        
-        if let temporaryDocument = tab.temporaryDocument {
-            temporaryDocument.getURL().uponQueue(.main, block: { tempDocURL in
-                // If we successfully got a temp file URL, share it like a downloaded file,
-                // otherwise present the ordinary share menu for the web URL.
-                if tempDocURL.isFileURL {
-                    share(url: tempDocURL)
-                } else {
-                    share(url: url)
-                }
-            })
-        } else {
-            share(url: url)
-        }
+        navigationHelper.openShareSheet()
     }
     
     func tabToolbarDidPressMenu(_ tabToolbar: ToolbarProtocol) {
@@ -3704,4 +3695,37 @@ extension BrowserViewController: OnboardingControllerDelegate {
     
     // 60 days until the next time the user sees the onboarding..
     static let onboardingDaysInterval = TimeInterval(60.days)
+}
+
+extension BrowserViewController: NSFetchedResultsControllerDelegate {
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        if #available(iOS 14.0, *) {
+            updateWidgetFavoritesData()
+        }
+    }
+    
+    @available(iOS 14.0, *)
+    private func updateWidgetFavoritesData() {
+        guard let frc = widgetBookmarksFRC else { return }
+        try? frc.performFetch()
+        if let favs = frc.fetchedObjects {
+            let group = DispatchGroup()
+            var favData: [WidgetFavorite] = []
+            favs.prefix(16).forEach { fav in
+                if let url = fav.url?.asURL {
+                    group.enter()
+                    let fetcher = FaviconFetcher(siteURL: url, kind: .largeIcon)
+                    widgetFaviconFetchers.append(fetcher)
+                    fetcher.load { _, attributes in
+                        favData.append(.init(url: url, favicon: attributes))
+                        group.leave()
+                    }
+                }
+            }
+            group.notify(queue: .main) { [self] in
+                widgetFaviconFetchers.removeAll()
+                FavoritesWidgetData.updateWidgetData(favData)
+            }
+        }
+    }
 }
